79e3e13c1b2a7cfa510d316aaf4822e2c26a88fb
Yoichiro Hasebe <yohasebe@gmail.com>
2023-08-23 18:55:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Whisper Voice-to-Text Stream Transcriber
+
+This project is a bash script that uses the [OpenAI Whisper API](https://platform.openai.com/docs/guides/speech-to-text) to transcribe stream audio into text. It records audio using SoX, detects silence, and transcribes the audio into text. The transcribed text is automatically copied to the system clipboard for immediate use and also saved in a text file for record keeping.
+
+## Installation
+
+1. Download the `whisper-stream.sh` script to your local machine.
+
+2. Make the script executable by running the following command:
+
+```bash
+chmod +x whisper-stream.sh
+```
+
+3. To make the script available universally, move it to `/usr/local/bin` directory:
+
+```bash
+sudo mv whisper-stream.sh /usr/local/bin
+```
+
+## Dependencies
+
+This script requires the following dependencies:
+
+- `curl`
+- `jq`
+- `sox`
+- `xclip` or `pbcopy` or `clip.exe` depending on your OS
+
+### Linux
+
+On a Debian-based Linux distribution, you can install these dependencies with:
+
+```bash
+sudo apt-get install curl jq sox xclip
+```
+
+### macOS
+
+On macOS, you can install these dependencies with Homebrew:
+
+```bash
+brew install curl jq sox
+```
+
+`pbcopy` is pre-installed on macOS.
+
+### Windows
+
+On Windows, you can install these dependencies with Chocolatey:
+
+```bash
+choco install curl jq sox
+```
+
+`clip.exe` is pre-installed on Windows.
+
+## Usage
+
+You can start the script with the following command:
+
+```bash
+./whisper-stream.sh [options]
+```
+
+The available options are:
+
+- `-v, --volume <value>`: Set the minimum volume threshold (default: 1%)
+- `-s, --silence <value>`: Set the minimum silence length (default: 1.5)
+- `-o, --oneshot`: Enable one-shot mode
+- `-d, --duration <value>`: Set the recording duration in seconds (default: 0, continuous)
+- `-t, --token <value>`: Set the OpenAI API token
+- `-p, --path <value>`: Set the output directory path
+- `-r, --prompt <value>`: Set the prompt for the API call (the prompt should match the audio language)
+- `-l, --language <value>`: Set the input language in ISO-639-1 format
+- `-h, --help`: Display the help message
+
+## Examples
+
+Here are some usage examples with a brief comment on each of them:
+
+`./whisper-stream.sh`
+
+This will start the script with the default settings, recording audio continuously and transcribing it into text using the default volume threshold and silence length. If the OpenAI API token is not provided as an argument, the script will automatically use the value of the `OPENAI_API_KEY` environment variable if it is set.
+
+`./whisper-stream.sh -v 2% -s 2 -o -d 60 -t your_openai_api_token -p /path/to/output/directory`
+
+This example sets the minimum volume threshold to 2%, the minimum silence length to 2 seconds, enables one-shot mode, sets the recording duration to 60 seconds, specifies the OpenAI API token, and sets the output directory path to `/path/to/output/directory`. If the OpenAI API token is not provided as an argument, the script will automatically use the value of the `OPENAI_API_KEY` environment variable if it is set.
---
+++ whisper-stream.sh
@@ -0,0 +1,270 @@
+#!/bin/bash
+
+# Set the default sensitivity values
+MIN_VOLUME=1%
+SILENCE_LENGTH=1.5
+ONESHOOT=false
+DURATION=0
+TOKEN=""
+OUTPUT_DIR=""
+PROMPT=""
+LANGUAGE=""
+
+# Function to display the help message
+function display_help() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -v, --volume <value>     Set the minimum volume threshold (default: 1%)"
+  echo "  -s, --silence <value>    Set the minimum silence length (default: 1.5)"
+  echo "  -o, --oneshot            Enable one-shot mode"
+  echo "  -d, --duration <value>   Set the recording duration in seconds (default: 0, continuous)"
+  echo "  -t, --token <value>      Set the OpenAI API token"
+  echo "  -p, --path <value>       Set the output directory path"
+  echo "  -r, --prompt <value>     Set the prompt for the API call"
+  echo "  -l, --language <value>   Set the language in ISO-639-1 format"
+  echo "  -h, --help               Display this help message"
+  exit 0
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -v|--volume)
+      MIN_VOLUME="$2"
+      shift
+      shift
+      ;;
+    -s|--silence)
+      SILENCE_LENGTH="$2"
+      shift
+      shift
+      ;;
+    -o|--oneshot)
+      ONESHOOT=true
+      shift
+      ;;
+    -d|--duration)
+      DURATION="$2"
+      shift
+      shift
+      ;;
+    -t|--token)
+      TOKEN="$2"
+      shift
+      shift
+      ;;
+    -p|--path)
+      OUTPUT_DIR="$2"
+      shift
+      shift
+      ;;
+    -r|--prompt)
+      PROMPT="$2"
+      shift
+      shift
+      ;;
+    -l|--language)
+      LANGUAGE="$2"
+      shift
+      shift
+      ;;
+    -h|--help)
+      display_help
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+# If no output directory is provided as an argument, set it to the current directory
+if [ -z "$OUTPUT_DIR" ]; then
+  OUTPUT_DIR="."
+fi
+
+# If no token is provided as an argument, try to get it from the environment variable
+if [ -z "$TOKEN" ]; then
+  TOKEN="${OPENAI_API_KEY:-}"
+fi
+
+# If no token is provided as an argument or environment variable, exit the script
+if [ -z "$TOKEN" ]; then
+  echo "No OpenAI API key provided. Please provide it as an argument or environment variable."
+  exit 1
+fi
+
+# Set the model for audio transcription
+MODEL="whisper-1"
+
+# Set the maximum number of retries
+MAX_RETRIES=3
+
+# Set the timeout duration in seconds
+TIMEOUT_SECONDS=5
+
+# Array to store the output file names
+output_files=()
+
+# Variable to store the accumulated text
+accumulated_text=""
+
+# Function to convert audio to text using the Whisper API
+function convert_audio_to_text() {
+  local output_file=$1
+  local curl_command="curl -s --request POST \
+    --url https://api.openai.com/v1/audio/transcriptions \
+    --header \"Authorization: Bearer $TOKEN\" \
+    --header \"Content-Type: multipart/form-data\" \
+    --form \"file=@$output_file\" \
+    --form \"model=$MODEL\" \
+    --form \"response_format=json\""
+
+  if [ -n "$PROMPT" ]; then
+    curl_command+=" --form \"prompt=$PROMPT\""
+  fi
+
+  if [ -n "$LANGUAGE" ]; then
+    curl_command+=" --form \"language=$LANGUAGE\""
+  fi
+
+  response=$(eval $curl_command)
+  # Check if the curl command was successful
+  if [ $? -ne 0 ]; then
+    echo "API call failed."
+    return 1
+  fi
+
+  transcription=$(echo "$response" | jq -r '.text')
+  
+  # Check if the transcription was successful
+  if [ $? -ne 0 ]; then
+    echo "Failed to parse the response."
+    return 1
+  fi
+
+  echo "$transcription"
+  
+  # Remove the output audio file
+  rm -f "$output_file"
+  
+  # Accumulate the transcribed text
+  accumulated_text+="$transcription
+"
+}
+
+# Function to handle script termination
+function handle_exit() {
+  # Kill all child processes
+  pkill -P $$
+
+  # Remove all output audio files
+  for file in "${output_files[@]}"; do
+    rm -f "$file"
+  done
+
+  # Create a text file with the accumulated text in the specified directory
+  timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+  echo "$accumulated_text" > "$OUTPUT_DIR/transcription_$timestamp.txt"
+
+  # Copy the accumulated text to the clipboard
+  case "$(uname -s)" in
+    Darwin)
+      echo "$accumulated_text" | pbcopy
+      ;;
+    Linux)
+      echo "$accumulated_text" | xclip -selection clipboard
+      ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+      # This is a rough guess that you're on Windows Subsystem for Linux
+      echo "$accumulated_text" | clip.exe
+      ;;
+    *)
+      echo "Unknown OS, cannot copy to clipboard"
+      ;;
+  esac
+
+  exit
+}
+
+# Function to retry API call
+function retry_api_call() {
+  local retries=0
+  local response=""
+  local output_file=$1
+  
+  while [ $retries -lt $MAX_RETRIES ]; do
+    echo "Attempt $((retries+1))..."
+    response=$(custom_timeout "$TIMEOUT_SECONDS" convert_audio_to_text "$output_file")
+    
+    if [ $? -eq 0 ]; then
+      echo "$response"
+      return 0
+    else
+      retries=$((retries + 1))
+      sleep 1
+    fi
+  done
+  
+  echo "API call failed after $MAX_RETRIES retries."
+  return 1
+}
+
+# Custom timeout function
+function custom_timeout() {
+  local timeout=$1
+  shift
+  
+  # Run the command in the background
+  "$@" &
+  local pid=$!
+  
+  # Wait for the command to finish or timeout
+  ( sleep "$timeout" && kill -9 "$pid" ) 2>/dev/null &
+  local watchdog=$!
+  
+  # Wait for the command or watchdog to finish
+  wait "$pid" 2>/dev/null
+  local result=$?
+  
+  # Clean up the watchdog process
+  kill -9 "$watchdog" 2>/dev/null
+  
+  return $result
+}
+
+# Trap SIGINT (Ctrl+C), SIGTSTP (Ctrl+Z) and call handle_exit()
+trap handle_exit SIGINT SIGTSTP
+
+# Start recording audio using SoX and detect silence
+while true; do
+  # Set the path to the output audio file
+  OUTPUT_FILE="output_$(date +%s).mp3"
+  
+  # Add the output file to the array
+  output_files+=("$OUTPUT_FILE")
+
+  # record audio in raw format then convert to mp3
+  if [ "$DURATION" -gt 0 ]; then
+    rec -q -V0 -e signed -L -c 1 -b 16 -r 44100 -t raw - trim 0 "$DURATION" silence 1 0.1 "$MIN_VOLUME" 1 "$SILENCE_LENGTH" "$MIN_VOLUME" | \
+    sox -t raw -r 44100 -b 16 -e signed -c 1 - "$OUTPUT_FILE"
+  else
+    rec -q -V0 -e signed -L -c 1 -b 16 -r 44100 -t raw \
+      - silence 1 0.1 "$MIN_VOLUME" 1 "$SILENCE_LENGTH" "$MIN_VOLUME" | \
+      sox -t raw -r 44100 -b 16 -e signed -c 1 - "$OUTPUT_FILE"
+  fi
+
+  # Check if the audio file is created successfully
+  if [ -s "$OUTPUT_FILE" ]; then
+    # Convert the MP3 audio to text using the Whisper API
+    convert_audio_to_text "$OUTPUT_FILE"
+  else
+    echo "No audio recorded."
+  fi
+
+  if [ "$ONESHOOT" = true ]; then
+    break
+  fi
+done
+
